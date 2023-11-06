9485777a1ca4fe8050403dfa8d59e681b3a02aeb
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-06 22:07:55 +0530

    Basic Add

---
+++ Basic.py
@@ -0,0 +1,17 @@
+# ----------------------------- Print Hello World  ------------------------------------
+
+print("Hello World")
+
+# ----------------------------- Make variables ------------------------------------
+
+# name="Shivam"
+# age=21
+# print(name)
+# print(age)
+# name="gupta"
+# print(name)
+
+# ---------------------- Taking Input   -------------------------------------------
+
+# name=input("What is your name :")
+# print("Your name is "+name)
