ec4f7bbb75362c8baf10abdc18c11843d2656f77
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-06 22:12:53 +0530

    Arithmetic Operator

---
+++ ArithmeticOperators.py
@@ -0,0 +1,18 @@
+# ---------------------- Arithmetic Operation  -------------------------------------------
+
+print("7+2 is "+str(7+2))
+print("7-2 is "+str(7-2))
+print("7/2 is "+str(7/2))
+print("7//2 is "+str(7//2))
+print("7%2 is "+str(7%2))
+print("7*2 is "+str(7*2))
+print("7**2 is "+str(7**2))
+
+print("5>2 is "+str(5>2))
+print("5>= 2 is "+str(5>=2))
+print("5<2 is "+str(5<2))
+print("5<=2 is "+str(5<=2))
+
+print(2>3 or 2>1)
+print(2>3 and 2>1)
+print(not 2>3)
