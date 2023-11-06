0a6e6c01064c48f04d523b90eb1afc8d65f91c1f
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-06 22:11:44 +0530

    String

---
+++ String.py
@@ -0,0 +1,21 @@
+
+
+# ---------------------- String   -------------------------------------------
+
+name="Shivam gupta"
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.find('G'))
+print(name.find('g'))
+
+# # -------------------------Strings are immutable i.e It cannot be changed
+# print(name)
+
+# ---------------------- String Methods  -------------------------------------------
+
+# name="Tony Stark"
+# print (name.replace("Tony Stark","Shivam Gupta"))
+# print(name)
+# print("S" in name)
+# print("s" in name)
