b2e4ea04d2ccf3d3f65b9c1e57f15d33f6bfe6a9
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-06 22:09:59 +0530

    Adding Integer

---
+++ AddingInteger.py
@@ -0,0 +1,12 @@
+# ---------------------- Add 2 number   -------------------------------------------
+
+first=input("Enter First number : ")
+second=input("Enter Second number : ")
+
+# sum=first+second
+
+# #------------- This add 2 string 
+# # -------------for addition of integer  we have to use 
+sum=int(first)+int(second)
+print(sum)
+print("The sum of "+first+" and "+second+"  number is :"+ str(sum))
