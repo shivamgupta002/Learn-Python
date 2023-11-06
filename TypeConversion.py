702d26ffdb9a4ee8b0b0fb1ea34183998d015112
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-06 22:08:56 +0530

    TypeCOnversion

---
+++ TypeConversion.py
@@ -0,0 +1,8 @@
+# ---------------------- Type Conversion   -------------------------------------------
+
+age=input("What is your age :")
+# new_age=age+5
+# give error because Input is in string and we add integer in string 
+# for correct this we use ->
+new_age=int(age)+5
+print(new_age)
