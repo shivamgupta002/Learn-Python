71db52e925711132ee34f236b73bf598f28e6b6f
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-16 18:49:43 +0530

    Loop Add

---
+++ Loop.py
@@ -0,0 +1,37 @@
+# ------------------- For Loop --------------------------
+
+# for i in range(10):
+#     print(i+1)
+
+# for i in range(10):
+#     if(i==8):
+#         continue
+#     print(i)
+
+# print("Printing set....")
+# a=[1,2,32,64,128,256]
+# s={3,4,5}
+
+# for number in a:
+#     print(number)
+
+# for item in s:
+#     print(item)
+    
+# ------------------- While Loop --------------------------
+
+# i=0
+# while(i<10):
+#     print(i)
+#     i+=1
+    
+# print("Program is finished...")
+
+# while(True):
+#     num=int(input("Enter number : "))
+#     print(num)
+#     if(num == 0):
+#         break
+# print("Program has finished...")
+
+
