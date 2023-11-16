b42c2df44768174396fc336fb048021975e0087a
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-16 19:21:25 +0530

    add more content on function

--- Function.py
+++ Function.py
@@ -9,11 +9,22 @@
 
 # ------------------- With Argument -------------------------
 
-def greetHello(name):
-    print("Hello "+name)
-    print("Hello")
-    print("Hello")
+# def greetHello(name):
+#     st=f"Hello ,Sir My name is {name} gupta"
+#     print(st)
+    
+#     print("Hello "+name)
+#     print("Hello")
+#     print("Hello")
 
-print("Start")
-greetHello("Shivam")
-print("Done")
+# print("Start")
+# greetHello("Shivam")
+# print("Done")
+
+# ------------------- With Return type -------------------------
+def average(a,b):
+    return (a+b)/2
+
+# print("Average of given number is "+str ( average(4,6)))
+# print( average(4,6))
+
